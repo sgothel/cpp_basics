1072359c83787e948ab275d2d7be6cb6af45dc6c
Sven Gothel <sgothel@jausoft.com>
2022-10-20 17:36:50 +0200

    Lesson 01: Complete task. Added array of callables (function pointer) using lambda to permutate through different geometric objects

--- src/lesson01.cpp
+++ src/lesson01.cpp
@@ -3,7 +3,7 @@
 // Author      : Sven Gothel
 // Version     : 0.1
 // Copyright   : MIT
-// Description : C++ Lesson 00
+// Description : C++ Lesson 01
 //============================================================================
 
 #include <cstdio>
@@ -15,6 +15,22 @@
  *
  * Application of C++ functions rendering geometric Objects using ASCII-Art
  */
+
+void print_space(const int n) {
+    for(int i = 0; i < n; ++i) {
+        printf(" "); // space
+    }
+}
+
+void print_mark(const int n, const char c='X') {
+    for(int i = 0; i < n; ++i) {
+        printf("%c", c);
+    }
+}
+
+void print_newline() {
+    printf("\n"); // newline or line-feed (lf)
+}
 
 /**
  * Pyramid w/ base_len == 4
@@ -29,6 +45,11 @@
 void pyramid_down(const int base_len, const int dx=0, const bool show_title=true) {
     if( show_title ) {
         printf("\nPyramid(down, l %d, dx %d)\n", base_len, dx);
+    }
+    for(int i=base_len; i > 0; i-=2) {
+        print_space( dx + ( base_len - i ) / 2 );
+        print_mark( i );
+        print_newline();
     }
 }
 
@@ -45,6 +66,12 @@ void pyramid_down(const int base_len, const int dx=0, const bool show_title=true
 void pyramid_up(const int base_len, const int dx=0, const bool show_title=true) {
     if( show_title ) {
         printf("\nPyramid(up, l %d, dx %d)\n", base_len, dx);
+    }
+    int i = 2 - ( base_len % 2 );
+    for(; i <= base_len; i+=2) {
+        print_space( dx + ( base_len - i ) / 2 );
+        print_mark( i );
+        print_newline();
     }
 }
 
@@ -65,6 +92,8 @@ void salino(const int base_len, const int dx=0, const bool show_title=true) {
     if( show_title ) {
         printf("\nSalino(l %d, dx %d)\n", base_len, dx);
     }
+    pyramid_up(base_len, dx, false);
+    pyramid_down(base_len-2, dx+1, false);
 }
 
 /**
@@ -107,6 +136,39 @@ void disk_1(const float radius, const int dx=0, const float sx=1.0, const bool s
     if( show_title ) {
         printf("\nDisk1(r %.2f, dx %d, sx %.2f)\n", radius, dx, sx);
     }
+    const float r_sq = radius*radius; // square of disk radius
+    const float disk_p0_x = radius - 0.5; // disk center point p0, x-component
+    const float disk_p0_y = radius - 0.5; // disk center point p0, y-component
+    const int aabbox_h = (int)std::floor(2 * radius); // disk AABBox height
+    const int aabbox_w = (int)std::floor(2 * radius * sx); // disk AABBox width
+    const float sx_r = aabbox_w / ( 2 * radius );
+
+    for(int y=0; y<aabbox_h; ++y) {
+        print_space(dx);
+        for(int x=0; x<aabbox_w; ++x) {
+            const float a = disk_p0_x - x/sx_r;
+            const float b = disk_p0_y - y;
+            const float dxy_p0_sq = a*a + b*b;
+            if( dxy_p0_sq <= r_sq ) {
+                print_mark( 1 );
+            } else {
+                print_space( 1 );
+            }
+        }
+        print_newline();
+    }
+}
+
+static std::string brightness("#=+-.");
+
+/**
+ * Return a char, representing given brightness.
+ * @param b brightness ranging from [0..1]
+ */
+char get_char(const float b) {
+    const size_t max_idx = brightness.size()-1;
+    const float b_n = std::max<float>( 0.0, std::min<float>(1.0 , b) ); // normalize [0..1]
+    return brightness[ std::min<size_t>(max_idx, std::round( b_n * max_idx ) ) ];
 }
 
 /**
@@ -151,6 +213,41 @@ void disk_2(const float radius, const int dx=0, const float sx=1.0, const float 
     if( show_title ) {
         printf("\nDisk2(r %.2f, dx %d, sx %.2f, aa_seam %.2f)\n", radius, dx, sx, aa_seam);
     }
+    const float disk_p0_x = radius - 0.5; // disk center point p0, x-component
+    const float disk_p0_y = radius - 0.5; // disk center point p0, y-component
+    const int aabbox_h = (int)std::floor(2 * radius); // disk AABBox height
+    const int aabbox_w = (int)std::floor(2 * radius * sx); // disk AABBox width
+    const float sx_r = aabbox_w / ( 2 * radius );
+
+    for(int y=0; y<aabbox_h; ++y) {
+        print_space(dx);
+        for(int x=0; x<aabbox_w; ++x) {
+            const float a = disk_p0_x - x/sx_r;
+            const float b = disk_p0_y - y;
+            const float dxy_p0 = std::sqrt( a*a + b*b );
+            if( dxy_p0 <= radius - aa_seam ) {
+                print_mark( 1, get_char( 0 ) );
+            } else if( dxy_p0 <= radius ) {
+                // dxy_p0 = ] ( r - aa_seam ) .. r ]
+                const float d_r = ( radius - dxy_p0 ) / aa_seam ; // d_r ~ 1/brightness
+                print_mark( 1, get_char( 1.0 - d_r ) );
+            } else {
+                print_space( 1 );
+            }
+        }
+        print_newline();
+    }
+}
+
+template<int max_diameter, int sx>
+void disk_2b(const int r) {
+    disk_2(r/2, max_diameter-r+1, sx);
+}
+
+typedef void(*paint_func)(const int p);
+
+void paint(const int p, paint_func pf) {
+    pf(p);
 }
 
 int main(int argc, const char* argv[]) {
@@ -182,6 +279,24 @@ int main(int argc, const char* argv[]) {
         for(int i=1; i<=max_size; ++i) {
             disk_1(i, max_size-i, 2);
             disk_2(i, max_size-i, 2);
+        }
+    }
+
+    {
+        const int sx = 2;
+        const int min_size =  3;
+        const int max_size = 10;
+        printf("\nAlternate object with size [%d..%d]\n", min_size, max_size);
+
+        paint_func p1 = [](const int p) { pyramid_up(p*sx, max_size-p); };
+        paint_func paint_funcs[] = { p1,
+                                     [](const int p) { pyramid_down(p*sx, max_size-p); },
+                                     disk_2b<max_size, sx> };
+
+        for(int i=min_size; i<=max_size; i++) {
+            for(size_t t=0; t<sizeof(paint_funcs)/sizeof(paint_func); ++t) {
+                paint(i, paint_funcs[t]);
+            }
         }
     }
 
